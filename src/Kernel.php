2940dbbaba228001067207979e56eb1dab89a958
cooldude77 <ashvinisaxena77@gmail.com>
2024-02-25 19:18:38 +0530

    Modifying class for location
    This class needs to be update the location of cache and logs in case of dev environment of ubuntu/vagrant

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,20 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    public function getCacheDir(): string
+    {
+        if($this->environment=="dev"){
+            return '/var/cache'; //magic happens here
+
+    }
+        return dirname(__DIR__).'/var/'.$this->environment.'/cache';
+}
+
+    public function getLogDir(): string
+    {
+        if($this->environment=="dev"){
+            return '/var/logs'; //and here
+    }
+        return dirname(__DIR__).'/var/'.$this->environment.'/logs';
+    }
 }
