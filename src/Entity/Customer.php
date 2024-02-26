ffd24df25f0bbce8751f3be72b696cf48e1ff908
cooldude77 <ashvinisaxena77@gmail.com>
2024-02-26 17:19:05 +0530

    Adding multiple tables
    Tables for currency, country etc added
    Migrations created

---
+++ src/Entity/Customer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CustomerRepository::class)]
+class Customer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $customerCode = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCustomerCode(): ?string
+    {
+        return $this->customerCode;
+    }
+
+    public function setCustomerCode(string $customerCode): static
+    {
+        $this->customerCode = $customerCode;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $productCode = null;
+
+    #[ORM\Column(length: 5000)]
+    private ?string $productDescription = null;
+
+   
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdProduct(): ?int
+    {
+        return $this->idProduct;
+    }
+
+    public function setIdProduct(int $idProduct): static
+    {
+        $this->idProduct = $idProduct;
+
+        return $this;
+    }
+
+    public function getProductCode(): ?string
+    {
+        return $this->productCode;
+    }
+
+    public function setProductCode(string $productCode): static
+    {
+        $this->productCode = $productCode;
+
+        return $this;
+    }
+
+    public function getProductDescription(): ?string
+    {
+        return $this->productDescription;
+    }
+
+    public function setProductDescription(string $productDescription): static
+    {
+        $this->productDescription = $productDescription;
+
+        return $this;
+    }
+
+}
