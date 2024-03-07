8208cd02b082e5d6278ae2179a6e612e0a727a20
cooldude77 <ashvinisaxena77@gmail.com>
2024-03-07 14:05:19 +0530

    Web shop product display page working

---
+++ src/Controller/Module/WebShop/External/Product/WebShopProductDisplayController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Module\WebShop\External\Product;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebShopProductDisplayController extends AbstractController
+{
+    #[Route('/shop/product/{code}', name: 'module_web_shop_product_display')]
+    public function home($code,
+                         ProductRepository $productRepository,
+                         Request $request): Response
+    {
+        $product = $productRepository->findOneBy(['productCode' => $code]);
+        return $this->render('module/web_shop/external/product/web_shop_product_display.html.twig',
+            ['product' => $product]);
+    }
+
+}
--- src/Controller/Module/WebShop/External/WebShopHomeController.php
+++ src/Controller/Module/WebShop/External/WebShopHomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Module\WebShop\External;
 
 use App\Repository\WebShopHomeRepository;
+use App\Repository\WebShopHomeSectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WebShopHomeController extends AbstractController
 {
-    #[Route('/shop/home', name: 'module_web_shop_home')]
-    public function home(WebShopHomeRepository $webShopHomeRepository, Request $request): Response
+    #[Route('/shop/home/{name}', name: 'module_web_shop_home')]
+    public function home(string                       $name, WebShopHomeRepository $webShopHomeRepository,
+                         WebShopHomeSectionRepository $webShopHomeSectionRepository,
+                         Request                      $request): Response
     {
-        $webShop = $webShopHomeRepository->findOneBy(['name' => 'Webshop']);
-        return $this->render('module/web_shop/external/web_shop.html.twig', ['webShop' => $webShop]);
+        $webShop = $webShopHomeRepository->findOneBy(['name' => $name]);
+        $webShopSection = $webShopHomeSectionRepository->findBy(['webShopHome' => $webShop]);
+        return $this->render('module/web_shop/external/web_shop_home.html.twig', ['webShopHome' => $webShop, 'webShopHomeSection' => $webShopSection]);
     }
 
 }
