4a04b79ad6412b00aa0dff6e447b3e83d929eaec
cooldude77 <ashvinisaxena77@gmail.com>
2024-03-09 18:58:22 +0530

    Restoring to before installing easy admin

--- src/Controller/Admin/UI/PanelController.php
+++ src/Controller/Admin/UI/PanelController.php
@@ -5,22 +5,25 @@ namespace App\Controller\Admin\UI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
 class PanelController extends AbstractController
 {
+    #[Route('/admin', name: 'admin_panel')]
     public function admin(
         Request         $request,
         RouterInterface $router): Response
     {
-        $content = "";
-        if ($request->get('load_next') !== null) {
 
+        if ($request->get('load_next') !== null) {
             $routeName = $request->get('load_next');
             $route = $router->getRouteCollection()->get($routeName);
             $controllerAction = $route->getDefault('_controller');
             $content = $this->forward($controllerAction)->getContent();
+
+            return $this->render('admin/ui/panel/panel.html.twig', ['content' => $content]);
         }
-        return $this->render('admin/ui/panel/panel.html.twig', ['content' => $content]);
+        return $this->render('admin/ui/panel/panel.html.twig', ['content' => "Hello"]);
     }
 }
