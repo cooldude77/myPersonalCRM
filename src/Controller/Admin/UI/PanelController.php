86d976d58f8d3cf4afe0994459d4276b600a12c9
cooldude77 <ashvinisaxena77@gmail.com>
2024-03-11 18:47:41 +0530

    Multiple changes to layout

--- src/Controller/Admin/UI/PanelController.php
+++ src/Controller/Admin/UI/PanelController.php
@@ -19,16 +19,55 @@ class PanelController extends AbstractController
         $adminUrl = $router->generate('admin_panel');
         $sideBar =
             [
-                'section' => [
-                    'header_text' => 'Product',
-                    'items' => [
-                        [
-                            'url' => $adminUrl . '?load_next=product_list&type=list',
-                            'text' => 'Product'
+                'sections' => [
+                    [
+                        'header_text' => 'Product',
+                        'items' => [
+                            [
+                                'url' => $adminUrl . '?load_next=product_list&type=list',
+                                'text' => 'Product List'
+                            ]
                         ]
-                    ]
+                    ],
+                    [
+                        'header_text' => 'Customer',
+                        'items' => [
+                            [
+                                'url' => $adminUrl . '?load_next=customer_list&type=list',
+                                'text' => 'Customer List'
+                            ]
+                        ]
+                    ],
+                    [
+                        'header_text' => 'Prices',
+                        'items' => [
+                            [
+                                'url' => $adminUrl . '?load_next=web_shop_list&type=list',
+                                'text' => 'Prices List'
+                            ]
+                        ]
+                    ],
+                    [
+                        'header_text' => 'WebShop',
+                        'items' => [
+                            [
+                                'url' => $adminUrl . '?load_next=web_shop_list&type=list',
+                                'text' => 'WebShop List'
+                            ]
+                        ]
+                    ], [
+                        'header_text' => 'Users',
+                        'items' => [
+                            [
+                                'url' => $adminUrl . '?load_next=web_shop_list&type=list',
+                                'text' => 'Users List'
+                            ]
+                        ]
+                    ],
 
-                ]
+
+                ],
+
             ];
 
 
