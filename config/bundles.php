e038e9b4185f0b6639ea3ce7555852116d92fe56
Ashvini <ashvinisaxena77@gmail.com>
2024-04-05 09:28:08 +0000

    Remvoing encore . Will use Assetmapper

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
