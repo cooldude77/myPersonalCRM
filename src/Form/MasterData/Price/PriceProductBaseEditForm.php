06225fbb0c90f557821297d4e0bb54d3cac4bf80
Ashvini <ashvinisaxena77@gmail.com>
2024-05-30 07:17:41 +0000

    All master data test cases working after adding the migration changes

--- src/Form/MasterData/Price/PriceProductBaseEditForm.php
+++ src/Form/MasterData/Price/PriceProductBaseEditForm.php
@@ -6,10 +6,12 @@ use App\Form\MasterData\Currency\CurrencyAutoCompleteField;
 use App\Form\MasterData\Price\DTO\PriceProductBaseDTO;
 use App\Form\MasterData\Product\ProductAutoCompleteField;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PriceProductBaseEditForm extends AbstractType
@@ -18,12 +20,30 @@ class PriceProductBaseEditForm extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('productId', ProductAutoCompleteField::class);
-        $builder->add('price', NumberType::class);
-        $builder->add('currencyId', CurrencyAutoCompleteField::class);
-        $builder->add('save', SubmitType::class);
+        $builder->add('id', ProductAutoCompleteField::class);
 
+        $builder->add('product', ProductAutoCompleteField::class, ['mapped' => false]);
+        $builder->add('price', NumberType::class);
+        $builder->add('currency', CurrencyAutoCompleteField::class, ['mapped' => false]);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $formEvent) {
+
+            $formEvent->getForm()->add('productId', HiddenType::class);
+            $formEvent->getForm()->add('currencyId', HiddenType::class);
+
+            // dto jugglery
+            $data = $formEvent->getData();
+
+            $data['productId'] = $data['product'];
+            $data['currencyId'] = $data['currency'];
+
+            $formEvent->setData($data);
+
+        });
+
+        $builder->add('save', SubmitType::class);
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
