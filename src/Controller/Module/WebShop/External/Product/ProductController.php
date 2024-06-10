87aba50f2e5f21e2459db90e331ee12e5ee337cd
Ashvini <ashvinisaxena77@gmail.com>
2024-06-10 09:09:37 +0000

    Single Product page now using panel framework

--- src/Controller/Module/WebShop/External/Product/ProductController.php
+++ src/Controller/Module/WebShop/External/Product/ProductController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Module\WebShop\External\Product;
 
+use App\Controller\Component\UI\Panel\Components\PanelContentController;
+use App\Controller\Component\UI\Panel\Components\PanelHeaderController;
+use App\Controller\Component\UI\PanelMainController;
+use App\Controller\Module\WebShop\External\Shop\HeaderController;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,9 +16,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     #[Route('/product/{name}', name: 'web_shop_product_single_display')]
-    public function single($name, ProductRepository $productRepository, Request $request): Response
+    public function mainPage($name, Request $request):
+    Response {
+
+        $session = $request->getSession();
+        
+        $session->set(
+            PanelHeaderController::HEADER_CONTROLLER_CLASS_NAME, HeaderController::class
+        );
+        $session->set(
+            PanelHeaderController::HEADER_CONTROLLER_CLASS_METHOD_NAME,
+            'header'
+        );
+        $session->set(
+            PanelContentController::CONTENT_CONTROLLER_CLASS_NAME, self::class
+        );
+        $session->set(
+            PanelContentController::CONTENT_CONTROLLER_CLASS_METHOD_NAME,
+            'single'
+        );
+        $request->query->set('name', $name);
+
+        return $this->forward(PanelMainController::class . '::main', ['request' => $request]);
+
+
+    }
+
+    public function single(ProductRepository $productRepository, Request $request): Response
     {
-        $product = $productRepository->findOneBy(['name' => $name]);
+
+        $product = $productRepository->findOneBy(['name' => $request->query->get('name')]);
         return $this->render(
             'module/web_shop/external/product/page/web_shop_single_product_display_page.html.twig',
             ['product' => $product]
