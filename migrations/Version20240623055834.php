4d1db8cf4ca2f900e424ecc4f47c28c26f3d4897
Ashvini <ashvinisaxena77@gmail.com>
2024-06-23 06:06:31 +0000

    Addresses is getting created in order
    Shipping and billing creation test case is working

---
+++ migrations/Version20240623055834.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240623055834 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_address CHANGE shipping_address_id shipping_address_id INT DEFAULT NULL, CHANGE billing_address_id billing_address_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE order_address CHANGE shipping_address_id shipping_address_id INT NOT NULL, CHANGE billing_address_id billing_address_id INT NOT NULL');
+    }
+}
